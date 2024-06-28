fed090dc253e6d6e2f5c8e66352ff44d09280191
Patricio Fuentealba <patricio.fuentealba.feliu@gmail.com>
2024-06-28 03:50:25 -0400

    allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^=6-_k)oh!n9-fpcd1qd0rf(!8y2!!8cc*so1if(!*ydv@*_dc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1','localhost','roca.onrender.com']
 
 
 # Application definition
