ceb938ee22b758af40c3e055ed064df6356dda28
Patricio Fuentealba <patricio.fuentealba.feliu@gmail.com>
2024-07-02 21:07:31 -0400

    se agrega nuevo site allowed_host

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 #DEBUG = True
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['DJANGO_ALLOWED_HOSTS','127.0.0.1','localhost','deslizza-app-2ezuf.ondigitalocean.app','deslizza-app-2nysq.ondigitalocean.app']
+ALLOWED_HOSTS = ['DJANGO_ALLOWED_HOSTS','127.0.0.1','localhost','deslizza-app-2ezuf.ondigitalocean.app','deslizza-app-2nysq.ondigitalocean.app','simulador.deslizza.site']
 
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
