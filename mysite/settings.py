50b2337bb4dccd56a86b1a57d77117dde7a27e3a
Patricio Fuentealba <patricio.fuentealba.feliu@gmail.com>
2024-08-26 09:57:07 -0400

    imagen: test10

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,7 +139,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "/workplace/roca_app/static"),
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "roca_app/static"),
 ]
 MEDIA_URL  = '/media/'
 MEDIA_ROOT  = os.path.join(BASE_DIR, "media")
